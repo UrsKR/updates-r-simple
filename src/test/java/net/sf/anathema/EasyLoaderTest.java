211e7b75c01938aa6c4953e9959e8ee0ef4b93a3
Urs Reupke <ur@idos.de>
2012-07-05 15:29:09 +0200

    Integrates folder-based classloader from Anathema project (http://anathema.github.com)

---
+++ src/test/java/net/sf/anathema/EasyLoaderTest.java
@@ -0,0 +1,32 @@
+package net.sf.anathema;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class EasyLoaderTest {
+
+  @Test
+  public void canLoadClassFromSingleJar() throws Exception {
+    EasyLoader loader = new EasyLoader(new File("./src/test/resources/jaxen-1.1.3.jar"));
+    loader.loadClass("org.jaxen.BaseXPath");
+  }
+
+  @Test
+  public void canLoadClassFromJarInFolder() throws Exception {
+    EasyLoader loader = new EasyLoader(new File("./src/test/resources/"));
+    loader.loadClass("org.jaxen.BaseXPath");
+  }
+
+  @Test
+  public void canLoadClassFromAnyJarInFolder() throws Exception {
+    EasyLoader loader = new EasyLoader(new File("./src/test/resources/"));
+    loader.loadClass("net.disy.commons.core.util.ArrayUtilities");
+  }
+
+  @Test
+  public void canLoadSystemClasses() throws Exception {
+    EasyLoader loader = new EasyLoader(new File("./src/test/resources/"));
+    loader.loadClass("java.lang.Math");
+  }
+}
