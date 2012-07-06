cf59fa44b2ae7c4ec4a457f6106fe1f8ff67eed6
Urs Reupke <ur@idos.de>
2012-07-06 10:40:23 +0200

    Cleans up version retrieval and adds errorhandling

--- src/main/java/de/idos/updates/HttpRepository.java
+++ src/main/java/de/idos/updates/HttpRepository.java
@@ -4,36 +4,39 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 public class HttpRepository implements Repository {
     private URL baseUrl;
 
-    public HttpRepository(String url) throws MalformedURLException {
-        this.baseUrl = new URL(url);
+    public HttpRepository(String url) {
+        try {
+            this.baseUrl = new URL(url);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Please specify a valid URL as your repository base.", e);
+        }
     }
 
     @Override
     public Version getLatestVersion() {
         try {
-            URL contentUrl = new URL(baseUrl, "updates/availableVersions");
-            InputStream input = contentUrl.openStream();
-            List<String> strings = IOUtils.readLines(input);
-            List<Version> versions = new VersionFactory().createVersionsFromStrings(strings);
-            return new VersionFinder().findLatestVersion(versions);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            throw new RuntimeException();
+            return readVersionsFromRepository();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            throw new RuntimeException();
+            return VersionFinder.BASE_VERSION;
         }
     }
 
     @Override
     public void transferVersionTo(Version version, VersionStore store) {
         //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private Version readVersionsFromRepository() throws IOException {
+        URL contentUrl = new URL(baseUrl, "updates/availableVersions");
+        InputStream input = contentUrl.openStream();
+        List<String> strings = IOUtils.readLines(input);
+        List<Version> versions = new VersionFactory().createVersionsFromStrings(strings);
+        return new VersionFinder().findLatestVersion(versions);
     }
 }
--- src/test/java/de/idos/updates/HttpRepositoryTest.java
+++ src/test/java/de/idos/updates/HttpRepositoryTest.java
@@ -16,4 +16,16 @@ public class HttpRepositoryTest {
         Version latestVersion = repository.getLatestVersion();
         assertThat(latestVersion, is(sameVersionAs(new NumericVersion(5, 0, 4))));
     }
+
+    @Test
+    public void returnsBaseVersionIfServerIsInaccessible() throws Exception {
+        HttpRepository repository = new HttpRepository("http://localhost:8080/updates");
+        Version latestVersion = repository.getLatestVersion();
+        assertThat(latestVersion, is(VersionFinder.BASE_VERSION));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptBadUrls() throws Exception {
+        new HttpRepository("xx");
+    }
 }
