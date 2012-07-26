eb419ef20cd5697dc2a4bf611a749fa4a058cce1
Urs Reupke <ur@idos.de>
2012-07-26 14:21:31 +0200

    Checks for updates on first method call. This allows clients to refer to the check e.g. in Listeners and react to the event that the version could not be determined. It is useful mainly for multi-threaded environments.

--- core/src/main/java/de/idos/updates/UpdateCheck.java
+++ core/src/main/java/de/idos/updates/UpdateCheck.java
@@ -4,45 +4,57 @@ import static de.idos.updates.UpdateAvailability.Available;
 import static de.idos.updates.UpdateAvailability.NotAvailable;
 
 public class UpdateCheck implements Updater {
-    private final Version currentVersion;
-    private final Version latestVersion;
-    private final UpdateConnection updateConnection;
+  private boolean checkHasRun = false;
+  private Version currentVersion;
+  private Version latestVersion;
+  private final UpdateConnection updateConnection;
 
-    public UpdateCheck(UpdateConnection updateConnection) {
-        this.updateConnection = updateConnection;
-        this.currentVersion = updateConnection.getLatestInstalledVersion();
-        this.latestVersion = updateConnection.getLatestAvailableVersion();
-    }
+  public UpdateCheck(UpdateConnection updateConnection) {
+    this.updateConnection = updateConnection;
+  }
 
-    @Override
-    public UpdateAvailability hasUpdate() {
-        if (latestVersion.isGreaterThan(currentVersion)) {
-            return Available;
-        }
-        return NotAvailable;
+  @Override
+  public UpdateAvailability hasUpdate() {
+    runCheck();
+    if (latestVersion.isGreaterThan(currentVersion)) {
+      return Available;
     }
+    return NotAvailable;
+  }
 
-    @Override
-    public Version getInstalledVersion() {
-        return currentVersion;
-    }
+  @Override
+  public Version getInstalledVersion() {
+    runCheck();
+    return currentVersion;
+  }
 
-    @Override
-    public Version getLatestVersion() {
-        if (!latestVersionIsNewerThanInstalledVersion()) {
-            return currentVersion;
-        }
-        return latestVersion;
+  @Override
+  public Version getLatestVersion() {
+    runCheck();
+    if (!latestVersionIsNewerThanInstalledVersion()) {
+      return currentVersion;
     }
+    return latestVersion;
+  }
 
-    @Override
-    public void updateToLatestVersion() {
-        if (latestVersionIsNewerThanInstalledVersion()) {
-            updateConnection.install(latestVersion);
-        }
+  @Override
+  public void updateToLatestVersion() {
+    runCheck();
+    if (latestVersionIsNewerThanInstalledVersion()) {
+      updateConnection.install(latestVersion);
     }
+  }
 
-    private boolean latestVersionIsNewerThanInstalledVersion() {
-        return hasUpdate() == Available;
+  @Override
+  public void runCheck() {
+    if (!checkHasRun) {
+      this.currentVersion = updateConnection.getLatestInstalledVersion();
+      this.latestVersion = updateConnection.getLatestAvailableVersion();
+      this.checkHasRun = true;
     }
+  }
+
+  private boolean latestVersionIsNewerThanInstalledVersion() {
+    return hasUpdate() == Available;
+  }
 }
--- core/src/test/java/de/idos/updates/UpdateCheckTest.java
+++ core/src/test/java/de/idos/updates/UpdateCheckTest.java
@@ -9,40 +9,40 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UpdateCheckTest {
-    Version currentVersion = new NumericVersion(1, 0, 0);
-    Version latestVersion = new NumericVersion(0, 0, 0);
-    UpdateConnection connection = mock(UpdateConnection.class);
+  Version currentVersion = new NumericVersion(1, 0, 0);
+  Version latestVersion = new NumericVersion(0, 0, 0);
+  UpdateConnection connection = mock(UpdateConnection.class);
 
-    @Before
-    public void setCurrentVersion() throws Exception {
-        when(connection.getLatestInstalledVersion()).thenReturn(currentVersion);
-    }
+  @Before
+  public void setCurrentVersion() throws Exception {
+    when(connection.getLatestInstalledVersion()).thenReturn(currentVersion);
+  }
 
-    @Test
-    public void returnsCurrentVersionIfNoNewerIsAvailable() throws Exception {
-        when(connection.getLatestAvailableVersion()).thenReturn(latestVersion);
-        Version version = getLatestVersion();
-        assertThat(version, is(currentVersion));
-    }
+  @Test
+  public void returnsCurrentVersionIfNoNewerIsAvailable() throws Exception {
+    when(connection.getLatestAvailableVersion()).thenReturn(latestVersion);
+    Version version = getLatestVersion();
+    assertThat(version, is(currentVersion));
+  }
 
-    @Test
-    public void doesNotChangeStateIfNewerVersionBecomesAvailableLater() throws Exception {
-        when(connection.getLatestAvailableVersion()).thenReturn(latestVersion, new NumericVersion(1,1,0));
-        Version version = getLatestVersion();
-        assertThat(version, is(currentVersion));
-    }
+  @Test
+  public void doesNotChangeStateIfNewerVersionBecomesAvailableLater() throws Exception {
+    when(connection.getLatestAvailableVersion()).thenReturn(latestVersion, new NumericVersion(1, 1, 0));
+    Version version = getLatestVersion();
+    assertThat(version, is(currentVersion));
+  }
 
-    @Test
-    public void publishesCurrentlyInstalledVersion() throws Exception {
-        assertThat(createUpdateCheck().getInstalledVersion(), is(currentVersion));
-    }
+  @Test
+  public void publishesCurrentlyInstalledVersion() throws Exception {
+    assertThat(createUpdateCheck().getInstalledVersion(), is(currentVersion));
+  }
 
-    private Version getLatestVersion() {
-        UpdateCheck updateCheck = createUpdateCheck();
-        return updateCheck.getLatestVersion();
-    }
+  private Version getLatestVersion() {
+    UpdateCheck updateCheck = createUpdateCheck();
+    return updateCheck.getLatestVersion();
+  }
 
-    private UpdateCheck createUpdateCheck() {
-        return new UpdateCheck(connection);
-    }
+  private UpdateCheck createUpdateCheck() {
+    return new UpdateCheck(connection);
+  }
 }
